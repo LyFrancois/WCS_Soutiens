ce8c31f88c10aab9c69c5b1ecb2f88038ad8034b
Ly françois <67745643+LyFrancois@users.noreply.github.com>
2020-11-15 18:26:16 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciceSoutien
+{
+    class Calculator                               
+    {
+        public float [] Integers { get; set; }
+
+       
+        public void Display()
+        {
+            foreach (float values in Integers) 
+            {
+                Console.WriteLine(values);
+            }
+        }
+
+        public float Sum()
+        {
+            float stock = 0;
+            for(int i =0;i < Integers.Length;i++)
+            {
+                stock = stock + Integers[i];
+            }
+
+            return stock;  
+        }
+
+        public float Average()
+        {
+            float average = Sum() / Integers.Length;
+            return average;
+        }
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExerciceSoutien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator calculator = new Calculator();
+
+            calculator.Integers = new float[4] { 1, 2, 4, 3 };
+            calculator.Display();
+            Console.WriteLine( "_\n"+"Somme : "+ calculator.Sum());
+            Console.WriteLine("_\n" + "Moyenne : " + calculator.Average());
+        }
+    
+    }
+}
